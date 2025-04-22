a114cb113d35830e00cb5d3f1f4cba7e90c700f4
ayinlade sikiru <ayinladesikiru@gmail.com>
2025-04-22 21:25:04 +0100

    verify fund endpoint added

--- wallet/urls.py
+++ wallet/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('welcome/', views.welcome, name='welcome'),
     path('greet/<str:name>', views.greeting, name='greet'),
     path('fund/account', views.fund_wallet, name='fund_wallet'),
+    path('fund/verify', views.verify_fund, name='verify_fund'),
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -2,14 +2,14 @@ from decimal import Decimal
 
 import requests
 from uuid import uuid4
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .serializers import FundSerializer
-from .models import Transaction
+from .models import Transaction, Wallet
 
 
 # Create your views here.
@@ -18,6 +18,7 @@ from .models import Transaction
 def welcome(request):
     return Response(f"welcome to django")
 
+@api_view()
 def greeting(request, name):
     return render(request, 'hello.html', {'name':name})
 
@@ -31,7 +32,7 @@ def fund_wallet(request):
     email = request.user.email
     reference = f"ref_{uuid4().hex}"
     Transaction.objects.create(
-        amount=amount,
+        amount=(amount/100),
         reference=reference,
         sender=request.user
     )
@@ -43,7 +44,8 @@ def fund_wallet(request):
     data = {
         "amount": amount,
         "reference": reference,
-        "email": email
+        "email": email,
+        "callback_url": "http://localhost:8000/wallet/fund/verify",
     }
     try:
         response_str = requests.post(url=url, json=data, headers=headers)
@@ -53,6 +55,31 @@ def fund_wallet(request):
         return None
     except requests.exceptions.RequestException as e:
         return Response({"message": f"Unable to complete transaction. {e}"}, status=status.HTTP_302_FOUND)
+
+@api_view()
+def verify_fund(request):
+    reference = request.GET.get('reference')
+    secret = settings.PAYSTACK_SECRET_KEY
+    headers = {
+        "Authorization": f"Bearer {secret}",
+    }
+    url = f"https://api.paystack.co/transaction/verify/{reference}"
+    response_str = requests.get(url=url, headers=headers)
+    response = response_str.json()
+    if response['status'] and response['data']['status'] == "success":
+        amount = (response['data']['amount'] / 100)
+        try:
+            transaction = Transaction.objects.get(reference=reference, verified=False)
+        except Transaction.DoesNotExist:
+            return Response({"message": "Transaction does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        wallet = get_object_or_404(Wallet, user=transaction.sender)
+        wallet.deposit(Decimal(amount))
+        wallet.save()
+        transaction.verified = True
+        transaction.save()
+        return Response({"message": "Deposit successful"}, status=status.HTTP_200_OK)
+    return Response({"message": "Transaction not successful"}, status=status.HTTP_404_NOT_FOUND)
+
 
 """
 curl https://api.paystack.co/transaction/initialize 
